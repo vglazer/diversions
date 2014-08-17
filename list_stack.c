8bd57c61fce4d327deee000849e3dceee5578cbf
Victor Glazer <victor.glazer@gmail.com>
2014-08-16 20:44:32 -0400

    Can insert at the front of the list now

--- list_stack.c
+++ list_stack.c
@@ -1,10 +1,26 @@
-typedef struct node {
+#include <stdlib.h>
+
+/* singly-linked list node */
+typedef struct node 
+{
     double value;
     struct node * next;
 } Node;
 
-int main(int argc, char const *argv[])
+/* insert a new node at the front of the list */
+Node *
+insert_front(Node * head, double value)
 {
-    /* code */
+    Node * new_head = malloc(sizeof(Node));
+    
+    new_head->value = value;
+    new_head->next = head;
+
+    return new_head;
+}
+
+/* driver */
+int main(int argc, char const * argv[])
+{
     return 0;
 }
