4228c1ee0658d319c9c9c4c23d897fe3031bb806
Victor Glazer <victor.glazer@gmail.com>
2014-08-17 19:37:33 -0400

    Added dispatch function

--- slist.c
+++ slist.c
@@ -1,18 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 1024
 
 /* singly-linked list node */
 typedef struct node 
 {
     double value;
-    struct node * next;
+    struct node *next;
 } Node;
 
 /* insert a new node at the front of the list */
-Node *
-insert_front(Node * head, double value)
+Node *insert_front(Node *head, double value)
 {
-    Node * new_head = malloc(sizeof(Node));
+    Node *new_head = malloc(sizeof(Node));
     
     new_head->value = value;
     new_head->next = head;
@@ -20,45 +22,64 @@ insert_front(Node * head, double value)
     return new_head;
 }
 
-/* print node values to stddout */
-void
-print_list(FILE * output_stream, Node * head)
+/*  node values to log */
+void print_list(Node *head, FILE *log)
 {
-    Node * curr;
+    Node *curr;
 
+    fprintf(log, "list contents\n");
     for (curr = head; curr != NULL; curr = curr->next)
     {
-        fprintf(output_stream, "%.2f\n", curr->value);
+        fprintf(log, "%.2f\n", curr->value);
     }
 }
 
-/* driver */
-int main(int argc, char const * argv[])
+/* parse commands file and invoke appropriate function, logging the results */
+Node *dispatch(Node **head_ptr, FILE *commands, char delim, FILE *log)
 {
-    Node * head;
-    FILE * output_stream;
+    char buffer[BUFSIZE], *temp;
+    double value;
 
-    /* output goes to stdout by default, but user can specify file */
-    if (argc > 2)
+    while (fgets(buffer, BUFSIZE, commands) != NULL)
     {
-        printf("usage: %s <input file> [output file]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    else if (argc == 2)
-    {
-        output_stream = fopen(argv[1], "w");
-    }
-    else
-    {
-        output_stream = stdout;
+        for (temp = buffer; *temp != '\0' && *temp != delim; ++temp);
+        if (*temp == '\0')
+        {
+            fprintf(stderr, "no delimeter found; skipping %s", buffer);
+            continue;
+        }
+
+        *temp = '\0';
+        ++temp;
+
+        value = atof(temp);
+        if (!strcmp(buffer, "insert_front"))
+        {
+            *head_ptr = insert_front(*head_ptr, value);
+            fprintf(log, "insert_front -> %f\n", value);
+        }
+        else
+        {
+            fprintf(stderr, "unknown command; skipping %s\n", buffer);
+            continue;
+        }
     }
 
-    head = insert_front(NULL, 5.0);
-    head = insert_front(head, 7.2);
+    return *head_ptr;
+}
 
-    print_list(output_stream, head);
+/* 
+    reads commands from stdin; writes results to stdout and errors to stderr.
+    sample usage: cat commands.txt  | ./slist > log.txt
+*/
+int main(int argc, char const *argv[])
+{
+    Node *head;
 
-    fclose(output_stream);
+    head = NULL;
+    head = dispatch(&head, stdin, ',', stdout);
+
+    print_list(head, stdout);
 
     return EXIT_SUCCESS;
 }
